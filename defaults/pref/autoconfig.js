b67759a92afee5876a5c68ec48b170e837e2d1c2
nmcspadden <nmcspadden@gmail.com>
2014-05-05 15:39:22 -0700

    Added better logging sample

--- defaults/pref/autoconfig.js
+++ defaults/pref/autoconfig.js
@@ -1,15 +1,18 @@
 
 Components.utils.import("file:///Users/admin/HorribleFirefoxIdea/macosx.js");
 
+var appInfo = Components.classes["@mozilla.org/xre/app-info;1"].getService(Components.interfaces.nsIXULAppInfo);
 macosx.importFramework("CoreFoundation", false);
 macosx.importFramework("Foundation", false);
 macosx.importFramework("AppKit", false);
 var message = macosx.CFStringCreateWithCString(null, "JS-MacOSX: JavaScript-Cocoa Bridge loaded into %@ v%@", macosx.kCFStringEncodingUTF8);
-macosx.NSLog(message);
-var prefName = macosx.CFStringCreateWithCString(null, "CatalogURL", macosx.kCFStringEncodingUTF8);
-var prefDomain = macosx.CFStringCreateWithCString(null, "com.apple.SoftwareUpdate", macosx.kCFStringEncodingUTF8);
+var appName = macosx.CFStringCreateWithCString(null, appInfo.name, macosx.kCFStringEncodingUTF8);
+var appVersion = macosx.CFStringCreateWithCString(null, appInfo.version, macosx.kCFStringEncodingUTF8);
+macosx.NSLog(message, appName, appVersion);
+var prefName = macosx.CFStringCreateWithCString(null, "homePage", macosx.kCFStringEncodingUTF8);
+var prefDomain = macosx.CFStringCreateWithCString(null, "org.mozilla.autocfg", macosx.kCFStringEncodingUTF8);
 var result = macosx.CFPreferencesCopyAppValue(prefName, prefDomain);
+
 var js_result = macosx.CFStringGetCStringPtr(result, macosx.CFStringGetFastestEncoding(result)).readString();
-var message = macosx.CFStringCreateWithCString(null, "JS-MacOSX: result", macosx.kCFStringEncodingUTF8);
-macosx.NSLog(message);
-macosx.NSLog(result);
+var message = macosx.CFStringCreateWithCString(null, "JS-MacOSX: Preference %@ result: %@", macosx.kCFStringEncodingUTF8);
+macosx.NSLog(message, prefName, result);
