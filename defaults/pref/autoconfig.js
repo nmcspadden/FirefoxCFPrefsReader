a5c1c2a2642ccfb8014f1a2adc2ea6e8577418c3
nmcspadden <nmcspadden@gmail.com>
2014-05-05 15:12:11 -0700

    Works in the browser console

--- defaults/pref/autoconfig.js
+++ defaults/pref/autoconfig.js
@@ -1,1 +1,15 @@
-Components.utils.import("/Users/nmcspadden/js-macosx-read-only/macosx.js")
+
+Components.utils.import("file:///Users/admin/HorribleFirefoxIdea/macosx.js");
+
+macosx.importFramework("CoreFoundation", false);
+macosx.importFramework("Foundation", false);
+macosx.importFramework("AppKit", false);
+var message = macosx.CFStringCreateWithCString(null, "JS-MacOSX: JavaScript-Cocoa Bridge loaded into %@ v%@", macosx.kCFStringEncodingUTF8);
+macosx.NSLog(message);
+var prefName = macosx.CFStringCreateWithCString(null, "CatalogURL", macosx.kCFStringEncodingUTF8);
+var prefDomain = macosx.CFStringCreateWithCString(null, "com.apple.SoftwareUpdate", macosx.kCFStringEncodingUTF8);
+var result = macosx.CFPreferencesCopyAppValue(prefName, prefDomain);
+var js_result = macosx.CFStringGetCStringPtr(result, macosx.CFStringGetFastestEncoding(result)).readString();
+var message = macosx.CFStringCreateWithCString(null, "JS-MacOSX: result", macosx.kCFStringEncodingUTF8);
+macosx.NSLog(message);
+macosx.NSLog(result);
