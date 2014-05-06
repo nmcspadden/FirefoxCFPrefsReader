66cfaeb109d00d89e8c4cf56bb1edadbd60c8933
Nick McSpadden <nmcspadden@gmail.com>
2014-05-05 21:22:00 -0700

    Baseline code for checking value and using it

--- defaults/pref/autoconfig.js
+++ defaults/pref/autoconfig.js
@@ -9,9 +9,18 @@ var message = macosx.CFStringCreateWithCString(null, "JS-MacOSX: JavaScript-Coco
 var appName = macosx.CFStringCreateWithCString(null, appInfo.name, macosx.kCFStringEncodingUTF8);
 var appVersion = macosx.CFStringCreateWithCString(null, appInfo.version, macosx.kCFStringEncodingUTF8);
 macosx.NSLog(message, appName, appVersion);
-var prefName = macosx.CFStringCreateWithCString(null, "homePage", macosx.kCFStringEncodingUTF8);
+
+//For each given preference, we need to check to see if it is forced, and then if it is, get the value
 var prefDomain = macosx.CFStringCreateWithCString(null, "org.mozilla.autocfg", macosx.kCFStringEncodingUTF8);
-var result = macosx.CFPreferencesCopyAppValue(prefName, prefDomain);
+
+//First, check the homePage
+var prefName = macosx.CFStringCreateWithCString(null, "homePage", macosx.kCFStringEncodingUTF8);
+var isForced = macosx.CFPreferencesAppValueIsForced(prefName, prefDomain);
+if (isForced) {
+	//If the value is forced, then we should copy the value to lock the pref with.
+	var result = macosx.CFPreferencesCopyAppValue(prefName, prefDomain);
+	Preferences.lock("browser.startup.homepage", result);
+}
 
 var js_result = macosx.CFStringGetCStringPtr(result, macosx.CFStringGetFastestEncoding(result)).readString();
 var message = macosx.CFStringCreateWithCString(null, "JS-MacOSX: Preference %@ result: %@", macosx.kCFStringEncodingUTF8);
